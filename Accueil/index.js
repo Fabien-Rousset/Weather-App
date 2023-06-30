c47cc5e06d472329a2e5130d5cfa4cbf29fe7a98
Fabien Rousset <fabien.rousset@icloud.com>
2023-06-30 10:18:20 +0200

    liaison des pages entre elles

--- Accueil/index.js
+++ Accueil/index.js
@@ -8,14 +8,29 @@ let accordion = document.getElementById("bouton");
         prevision.classList.toggle("noactive");
 	});
 
-
-
-
+/**
+ *          Ouverture de la page Random depuis "menu nuage"
+ *          de la page d'accueil
+ */
     let menuNuage = document.getElementById('menu-nuage');
 
     menuNuage.addEventListener('click', openWindow)
 
     function openWindow(){
-        fenetre = window.open("/Random/random2.html", "_self");
+        fenetre = window.open("/Favoris/favoris2.html", "_self");
     }
+
+
+
+/**
+ *          Ouverture de la page Favoris depuis "menu tete"
+ *          de la page d'accueil
+*/
+    let menuTete = document.getElementById("menu-tete");
+
+		menuTete.addEventListener("click", openWindow);
+
+		function openWindow() {
+			fenetre = window.open("/Random/random2.html", "_self");
+		}
 
