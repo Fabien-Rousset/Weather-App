80bb40c107c221274ee542a157b4b66f1542de7a
Fabien Rousset <fabien.rousset@icloud.com>
2023-07-05 08:16:57 +0200

    ajout du bouton Recherche JS

--- parametres.js
+++ parametres.js
@@ -1,29 +1,73 @@
 const apiKey = "79937de35840113f46750d7bb8a9171a";
-const url ="https://api.openweathermap.org/data/2.5/weather?q=dijon,fr&APPID=79937de35840113f46750d7bb8a9171a";
 
 
 
-fetch(url)
-.then(response => response.json())
-// console.log(response)
-.then((data) => {console.log(data);
-    console.log(data.name);
-    function renduMeteo(meteo){
-    let resultsContainer = document.getElementById("result-weather");
+
+ function renduMeteo(meteo){
+   
   
-    let ville = document.createElement('h2');
-    ville.textContent = meteo.name;
-    console.log(meteo.name);
-    resultsContainer.append(ville);
-    console.log(ville);
-};
-renduMeteo(data)
-console.log(data.main.temp)
+    // let resultat = document.createElement("div");
+    // resultat.textContent = meteo.main.temp;
+    // document.getElementById("position-actuelle").append(resultat);
+    // console.log(resultat);
+    // console.log(meteo);
+
+    let temp = document.getElementById('temperature');
+    temp.innerHTML = `${Math.round(meteo.main.temp)}°`;
+
+    let nomVille = document.getElementById("villeActuelle");
+    nomVille.innerHTML = `${meteo.name}`;
+
+    let tempMin = document.getElementById("temp-min");
+    tempMin.innerHTML = `Min ${Math.round(meteo.main.temp_min)}°`;
+    console.log(tempMin);
+
+    let tempMax = document.getElementById('temp-max');
+    tempMax.innerHTML = `&nbsp / Max ${Math.round(meteo.main.temp_max)}°`;
+
+
+
+
+   
 }
 
+ document.getElementById('appel_api').addEventListener("change", function(){
 
+    let pays = "fr";
+    let ville = document.getElementById("appel_api").value;
+    let url ="https://api.openweathermap.org/data/2.5/weather?q="+ville+","+pays+"&APPID="+apiKey+"&units=metric";
 
+        fetch(url)
+    .then(response => response.json())
+    // console.log(response)
+    .then((data) => {console.log(data);
+        console.log(data);
+        renduMeteo(data)
+        console.log(data.main.temp)
+
+}
+)
+}
 );
+
+// let reponseOk = function(data){
+
+    
+//     console.log(data)
+//     alert(data.main.temp)
+// }
+
+
+
+// function coordonnees(pos) {
+//     let crd = pos.coords;
+  
+//     let latitude = crd.latitude;
+//     let longitude = crd.longitude;
+// }
+
+// let positionActuelle = navigator.geolocation.getCurrentPosition(coordonnees)
+// console.log(positionActuelle)
 
 
 
